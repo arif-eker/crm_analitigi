ddd1818bb40676056c60d1159275521eeb212cae
Arif Eker <eng.arifeker@gmail.com>
2021-02-09 14:14:09 +0300

    CLTV Predict fonksiyonu eklendi.
    Müşterilerin 1 ve 3 aylık, bg_nbd satın alım tahminlerini yapan, daha sonra ise gamma_gamma ile birleşerek 6 aylık bir CLTV puanı tahmin eden fonksiyon eklendi.

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -134,3 +134,88 @@ def create_cltv_c(dataframe):
                            "cltv_c", "cltv_c_segment"]]
 
     return dataframe
+
+
+
+def create_cltv_p(dataframe):
+    today_date = dt.datetime(2011, 12, 11)
+
+    # recency kullanıcıya özel dinamik.
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': [lambda date: (date.max() - date.min()).days,
+                                                                lambda date: (today_date - date.min()).days],
+                                                'Invoice': lambda num: num.nunique(),
+                                                'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    rfm.columns = rfm.columns.droplevel(0)
+
+    # recency_cltv_p
+    rfm.columns = ['recency_cltv_p', 'T', 'frequency', 'monetary']
+
+    # basitleştirilmiş monetary_avg
+    rfm["monetary"] = rfm["monetary"] / rfm["frequency"]
+
+    rfm.rename(columns={"monetary": "monetary_avg"}, inplace=True)
+
+    # BGNBD için WEEKLY RECENCY VE WEEKLY T'nin HESAPLANMASI
+    # recency_weekly_cltv_p
+    rfm["recency_weekly_cltv_p"] = rfm["recency_cltv_p"] / 7
+    rfm["T_weekly"] = rfm["T"] / 7
+
+    # KONTROL
+    rfm = rfm[rfm["monetary_avg"] > 0]
+
+    # recency filtre (daha saglıklı cltvp hesabı için)
+    rfm = rfm[(rfm['frequency'] > 1)]
+
+    rfm["frequency"] = rfm["frequency"].astype(int)
+
+    # BGNBD
+    bgf = BetaGeoFitter(penalizer_coef=0.01)
+    bgf.fit(rfm['frequency'],
+            rfm['recency_weekly_cltv_p'],
+            rfm['T_weekly'])
+
+    # exp_sales_1_month
+    rfm["exp_sales_1_month"] = bgf.predict(4,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+    # exp_sales_3_month
+    rfm["exp_sales_3_month"] = bgf.predict(12,
+                                           rfm['frequency'],
+                                           rfm['recency_weekly_cltv_p'],
+                                           rfm['T_weekly'])
+
+    # expected_average_profit
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(rfm['frequency'], rfm['monetary_avg'])
+    rfm["expected_average_profit"] = ggf.conditional_expected_average_profit(rfm['frequency'],
+                                                                             rfm['monetary_avg'])
+    # 6 aylık cltv_p
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       rfm['frequency'],
+                                       rfm['recency_weekly_cltv_p'],
+                                       rfm['T_weekly'],
+                                       rfm['monetary_avg'],
+                                       time=6,
+                                       freq="W",
+                                       discount_rate=0.01)
+
+    rfm["cltv_p"] = cltv
+
+    # minmaxscaler
+    scaler = MinMaxScaler(feature_range=(1, 100))
+    scaler.fit(rfm[["cltv_p"]])
+    rfm["cltv_p"] = scaler.transform(rfm[["cltv_p"]])
+
+    # rfm.fillna(0, inplace=True)
+
+    # cltv_p_segment
+    rfm["cltv_p_segment"] = pd.qcut(rfm["cltv_p"], 3, labels=["C", "B", "A"])
+
+    # recency_cltv_p, recency_weekly_cltv_p
+    rfm = rfm[["recency_cltv_p", "T", "monetary_avg", "recency_weekly_cltv_p", "T_weekly",
+               "exp_sales_1_month", "exp_sales_3_month", "expected_average_profit",
+               "cltv_p", "cltv_p_segment"]]
+
+    return rfm
