e2c1cc8f548abee59d1f0bd00d04f0b2cbcca304
Arif Eker <eng.arifeker@gmail.com>
2021-02-09 14:04:33 +0300

    RFM işlemi yapıldı.
    Müşteriler için RFM tablosunu oluşturan fonksiyon eklendi.

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -59,3 +59,44 @@ def crm_data_prep(dataframe):
 def create_invoice_product_df(dataframe):
     return dataframe.groupby(['Invoice', 'StockCode'])['Quantity'].sum().unstack().fillna(0). \
         applymap(lambda x: 1 if x > 0 else 0)
+
+
+def create_rfm(dataframe):
+    # RFM METRIKLERININ HESAPLANMASI
+    # Dikkat! RFM için frekanslar nunique.
+
+    today_date = dt.datetime(2011, 12, 11)
+
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
+                                                'Invoice': lambda num: num.nunique(),
+                                                "TotalPrice": lambda price: price.sum()})
+
+    rfm.columns = ['recency', 'frequency', "monetary"]
+
+    rfm = rfm[(rfm['monetary'] > 0)]
+
+    # RFM SKORLARININ HESAPLANMASI
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+
+    # Monetary segment tanımlamada kullanılmadığı için işlemlere alınmadı.
+
+    # SEGMENTLERIN ISIMLENDIRILMESI
+    rfm['rfm_segment'] = rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str)
+
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+
+    rfm['rfm_segment'] = rfm['rfm_segment'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "rfm_segment"]]
+    return rfm
