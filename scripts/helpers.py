5d903bc3c2f4f362c62d8cdc13307012309f3ce3
Arif Eker <eng.arifeker@gmail.com>
2021-02-09 14:01:00 +0300

    argparse CLI's tanımlandı.

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -4,3 +4,13 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from lifetimes import BetaGeoFitter
 from lifetimes import GammaGammaFitter
+
+
+
+def get_namespace():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', dest='debug', action='store_true')
+    parser.add_argument('--no-debug', dest='debug', action='store_false')
+    parser.set_defaults(debug=True)
+    return parser.parse_args()
