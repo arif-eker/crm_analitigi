71cf2ad2f85589a2650d5918644eb65878d553fa
Arif Eker <eng.arifeker@gmail.com>
2021-02-09 14:40:58 +0300

    Timer contexi eklendi.
    Modüllerin ne kadar sürede işlem gördüğünü göstermek için timer eklendi.

--- crm_main.py
+++ crm_main.py
@@ -11,3 +11,11 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 pd.set_option('display.expand_frame_repr', False)
+
+
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
